45776e29738beb30d08c6d1b26261328cc8bdf69
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-28 22:28:20 +0545

    Initialize Project

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(InstagramClone());
+
+class InstagramClone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
