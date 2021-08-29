590d8823d4b1e51e65a851c91bbdc478ff17e022
Sandesh Rimal <rimal.sandesh11@gmail.com>
2021-08-29 22:30:32 +0545

    Added Photo Post section

--- lib/post.dart
+++ lib/post.dart
@@ -2,35 +2,39 @@ import 'package:flutter/material.dart';
 
 class Posts extends StatelessWidget {
   Widget instaPost(String asset, String name, String image) {
-    return Stack(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0, top: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('$asset'),
-                    radius: 15,
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    '$name',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, top: 10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('$asset'),
+                      radius: 15,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      '$name',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10.0, right: 8.0),
-              child: Icon(Icons.more_vert),
-            ),
-          ],
-        ),
-      ],
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, right: 8.0),
+                child: Icon(Icons.more_vert),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Image.asset('$image'),
+        ],
+      ),
     );
   }
 
